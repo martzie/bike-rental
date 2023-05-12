d2663489bd4e220c84cce011909ae923965802c7
marta <martzie@>
2023-05-12 21:51:25 +0200

    Separated client request validation messages to properties file

--- src/main/java/com/martzie/bikerental/client/controller/dto/ClientRequest.java
+++ src/main/java/com/martzie/bikerental/client/controller/dto/ClientRequest.java
@@ -9,35 +9,38 @@ import lombok.Getter;
 @Builder
 public class ClientRequest {
 
-    @Email(message = "Invalid email: Email must be in user@domain.com format")
-    @NotBlank(message = "Invalid email: Email cannot be empty")
+    @Email(message = "{email.format}")
+    @NotBlank(message = "{email.notempty}")
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "user@domain.com")
     private String emailAddress;
 
-    @NotBlank(message = "Invalid Name: cannot be empty")
-    @NotNull(message = "Invalid Name: cannot be null")
-    @Size(min = 3, max = 30, message = "Invalid Name: Must be of 3 - 30 characters")
+    @NotBlank(message = "{name.notempty}")
+    @NotNull(message = "{name.notnull}")
+    @Size(min = 3, max = 30, message = "{name.size}")
     @Schema(example = "John")
     private String firstName;
 
-    @Schema(requiredMode = Schema.RequiredMode.REQUIRED, example = "Smith", minLength = 3, maxLength = 20)
+    @NotBlank(message = "{lastname.notempty}")
+    @NotNull(message = "{lastname.notnull}")
+    @Size(min = 3, max = 30, message = "{lastname.size}")
+    @Schema(example = "Smith")
     private String lastName;
 
-    @Size(min = 3, max = 30, message = "Invalid city name: Must be of 3 - 30 characters")
+    @Size(min = 3, max = 30, message = "{city.name.size}")
     private String city;
 
-    @Size(min = 3, max = 50, message = "Invalid street name: Must be of 3 - 50 characters")
+    @Size(min = 3, max = 50, message = "{street.name.size}")
     private String street;
 
-    @NotBlank(message = "Invalid street number: cannot be blank")
+    @NotBlank(message = "{street.number.notempty}")
     private String streetNumber;
 
-    @Pattern(regexp = "^\\d{2}[- ]?\\d{3}$", message = "Postcode must be in 5 digit format ex. 00-000 or 00000")
+    @Pattern(regexp = "^\\d{2}[- ]?\\d{3}$", message = "{postcode.format}")
     private String postcode;
 
-    @NotNull(message = "Invalid phone number: cannot be null")
-    @NotBlank(message = "Invalid phone number: cannot be empty")
-    @Pattern(regexp = "^\\d{8,10}$", message = "Phone number must contain 8-10 digits")
+    @NotNull(message = "{phonenumber.notnull}")
+    @NotBlank(message = "{phonenumber.notempty}")
+    @Pattern(regexp = "^\\d{8,10}$", message = "{phonenumber.format}")
     private String phoneNumber;
 
 }
